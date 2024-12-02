f59777bb41f1ff7cc614871b2a915ff9ecd756fd
enriqueomarfarias <enrikefarias@gmail.com>
2024-12-02 18:10:23 -0300

    Finalizando desktop

--- article-preview-component-master/main.js
+++ article-preview-component-master/main.js
@@ -1,8 +1,1 @@
-const contenedor = document.getElementById("conteiner");
 
-contenedor.addEventListener("click", (e) => {
-  e.preventDefault();
-});
-
-var compartir = document.getElementById("share");
-share_btn.addEventListener("click", share);
